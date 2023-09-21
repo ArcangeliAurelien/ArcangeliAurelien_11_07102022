30c7017d5c236281dbe1acac374fdd1fb8be686c
Aurélien Arcangeli <arkaurel5929@gmail.com>
2023-09-21 15:51:43 +0200

    modif

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import '../styles/App.css';
 import '../styles/Body.css'
 import { Link } from 'react-router-dom'
-// import LogementList  from '../datas/LogementList'
 import { LogementList } from "../datas/LogementList";
 import Header from '../components/Header';
 import Banner from '../components/Banner';
