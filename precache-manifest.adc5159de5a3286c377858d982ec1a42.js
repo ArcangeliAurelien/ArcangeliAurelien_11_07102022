c668d64c62c0e347aca8c735af8c7a425132611f
Aurélien Arcangeli <arkaurel5929@gmail.com>
2023-09-25 16:29:06 +0200

    Updates

---
+++ precache-manifest.adc5159de5a3286c377858d982ec1a42.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d37735634fd44df7c3fe",
+    "url": "/static/css/main.be7c104f.chunk.css"
+  },
+  {
+    "revision": "d37735634fd44df7c3fe",
+    "url": "/static/js/main.d3773563.chunk.js"
+  },
+  {
+    "revision": "81fafdf2125209299671",
+    "url": "/static/js/1.81fafdf2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ba8c4ac8303c79335039c430399b2d28",
+    "url": "/static/media/arrowDown.ba8c4ac8.svg"
+  },
+  {
+    "revision": "309981e80eb2ae33d28946ec2dafaf2f",
+    "url": "/static/media/logo_Kasa.309981e8.png"
+  },
+  {
+    "revision": "6a79bf4a5e31e01c7c7a6005cfc2af8d",
+    "url": "/static/media/IMG.6a79bf4a.png"
+  },
+  {
+    "revision": "3cb52ced91e446ada050ab1f6e937232",
+    "url": "/static/media/IMG2.3cb52ced.png"
+  },
+  {
+    "revision": "b773a356fe17061cf035599cfaa09864",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ArcangeliAurelien_11_07102022/precache-manifest.75a588f3b12fcfd4251bf3404b683cb3.js"
+  "/precache-manifest.adc5159de5a3286c377858d982ec1a42.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ArcangeliAurelien_11_07102022/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
