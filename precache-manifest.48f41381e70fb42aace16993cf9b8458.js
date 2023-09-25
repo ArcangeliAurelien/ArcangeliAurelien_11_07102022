0865bd8ecb15b5305e71428eadf5a59b98aedc59
Aurélien Arcangeli <arkaurel5929@gmail.com>
2023-09-25 16:04:02 +0200

    Updates

---
+++ precache-manifest.48f41381e70fb42aace16993cf9b8458.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "411e58bbaa523738a72a",
+    "url": "https://arcangeliaurelien.github.io/ArcangeliAurelien_11_07102022/static/css/main.be7c104f.chunk.css"
+  },
+  {
+    "revision": "411e58bbaa523738a72a",
+    "url": "https://arcangeliaurelien.github.io/ArcangeliAurelien_11_07102022/static/js/main.411e58bb.chunk.js"
+  },
+  {
+    "revision": "81fafdf2125209299671",
+    "url": "https://arcangeliaurelien.github.io/ArcangeliAurelien_11_07102022/static/js/1.81fafdf2.chunk.js"
+  },
+  {
+    "revision": "888fafb61c772c92efec",
+    "url": "https://arcangeliaurelien.github.io/ArcangeliAurelien_11_07102022/static/js/runtime~main.888fafb6.js"
+  },
+  {
+    "revision": "ba8c4ac8303c79335039c430399b2d28",
+    "url": "https://arcangeliaurelien.github.io/ArcangeliAurelien_11_07102022/static/media/arrowDown.ba8c4ac8.svg"
+  },
+  {
+    "revision": "309981e80eb2ae33d28946ec2dafaf2f",
+    "url": "https://arcangeliaurelien.github.io/ArcangeliAurelien_11_07102022/static/media/logo_Kasa.309981e8.png"
+  },
+  {
+    "revision": "6a79bf4a5e31e01c7c7a6005cfc2af8d",
+    "url": "https://arcangeliaurelien.github.io/ArcangeliAurelien_11_07102022/static/media/IMG.6a79bf4a.png"
+  },
+  {
+    "revision": "3cb52ced91e446ada050ab1f6e937232",
+    "url": "https://arcangeliaurelien.github.io/ArcangeliAurelien_11_07102022/static/media/IMG2.3cb52ced.png"
+  },
+  {
+    "revision": "a3e138364b13ec7375513fb7a58c547f",
+    "url": "https://arcangeliaurelien.github.io/ArcangeliAurelien_11_07102022/index.html"
+  }
+];
